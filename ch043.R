04a32a28cb6af8a71343279cba52bb4a076d1e03
hongjung <trampsta@siu.edu>
2015-07-09 17:23:14 -0700

    7/9

---
+++ ch043.R
@@ -0,0 +1,26 @@
+x<-3
+x
+y<-3.0
+y
+class(x)
+is.numeric(x)
+
+X<-11L
+
+class(4)
+class(4L)
+class(2.8)
+4L * 28
+4L * 2.8
+class(4L * 2.8)
+5L/2L
+class(5L/2L)
+firstName <- "hong"
+
+firstName
+
+nchar(firstName)
+nchar("hong gyoon")
+nchar(2015)
+
+lastname <- factor("jung")
